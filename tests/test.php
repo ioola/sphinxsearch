245a0f8e4994359fff04e0d61e9176170e98aa63
casper <kasp@qq.com>
2020-06-04 10:45:30 +0800

    ok

--- tests/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../vendor/autoload.php'; // 加载自动加载文件
-
-use ioola\SphinxSearch\SphinxSearch;
-
-
-$sphinx = new SphinxSearch();
-$results = $sphinx->search('my query', 'index_name')->query();
